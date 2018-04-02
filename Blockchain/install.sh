2a97ba734824a9fd1bd7bcfe8a29a89b0c10e09c
Josh Snider <Joshua Snider>
2018-04-01 23:05:36 -0700

    Jumping around a bit...

---
+++ Blockchain/install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo add-apt-repository -y ppa:ethereum/ethereum-dev
+sudo apt-get update
+sudo apt-get install ethereum
